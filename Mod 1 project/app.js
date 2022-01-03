76fbb71bcfb9a89008fbe2cfc456cd87b3c889a7
gymbundye <84460968+gymbundye@users.noreply.github.com>
2022-01-03 15:47:29 -0500

    Add files via upload

--- Mod 1 project/app.js
+++ Mod 1 project/app.js
@@ -85,3 +85,40 @@ window.onclick = function(event) {
 //newMovies.addEventListener("click"){
 //  fetch()
 //}
+// let response = fetch("https://api.themoviedb.org/3/movie/23719?api_key=0af5b4f32534825e575111d5029fb03e")
+  //  document.getElementById("overview").innerHTML = response;
+   // api url
+const api_url = "https://api.themoviedb.org/3/movie/23719?api_key=0af5b4f32534825e575111d5029fb03e";
+
+// Defining async function
+async function getapi(url) {
+
+// Storing response
+const response = await fetch(url);
+
+// Storing data in form of JSON
+var data = await response.json();
+console.log(data);
+if (response) {
+  hideloader();
+}
+show(data);
+}
+// Calling that async function
+getapi(api_url);
+
+// Function to hide the loader
+function hideloader() {
+document.getElementById('loading').style.display = 'none';
+}
+// Function to define innerHTML for HTML table
+function show(data) {
+let tab = data.overview;
+  
+
+// Setting innerHTML as tab variable
+document.getElementById("overview").innerHTML = tab;
+}
+
+   debugger;
+
